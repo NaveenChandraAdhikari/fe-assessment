141e02aa48299c7b3972dd7fe155bce9a0854a4d
NaveenChandraAdhikari <nitinadhikari890@gmail.com>
2025-07-14 12:43:28 +0530

    deleted unused fun

--- assets/js/test.js
+++
@@ -1,8 +0,0 @@
-let words = [123, 4214, 1421, 4, 42, 41];
-
-function getRandomWords(words, count) {
-    const shuffled = words.sort(() => 0.5 - Math.random());
-    return shuffled.slice(0, count);
-}
-
-console.log(getRandomWords(words, 1));
